b759b5486f27f71347efcebe5221e115d71abe00
SoonYu97 <yusoon.97417@gmail.com>
2024-07-14 20:11:58 +0800

    feat: change 3 lanes into 5 lanes, refactor InputManager.cs

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -8,22 +8,12 @@ namespace DefaultNamespace
 {
     public class InputManager : MonoBehaviour
     {
+        public int LaneCount = 5;
+        
         private VisualElement root;
-        private VisualElement lane1;
-        private VisualElement lane2;
-        private VisualElement lane3;
-        private Button lane1Player1SpawnButton;
-        private Button lane2Player1SpawnButton;
-        private Button lane3Player1SpawnButton;
-        private Button lane1Player2SpawnButton;
-        private Button lane2Player2SpawnButton;
-        private Button lane3Player2SpawnButton;
-        private Action lane1Player1SpawnButtonClicked;
-        private Action lane2Player1SpawnButtonClicked;
-        private Action lane3Player1SpawnButtonClicked;
-        private Action lane1Player2SpawnButtonClicked;
-        private Action lane2Player2SpawnButtonClicked;
-        private Action lane3Player2SpawnButtonClicked;
+        private VisualElement[] lanes;
+        private Button[][] spawnButtons;
+        private Action[][] spawnButtonClicked;
         
         private LaneSystem laneSystem;
 
@@ -33,46 +23,41 @@ namespace DefaultNamespace
             
             root = GetComponent<UIDocument>().rootVisualElement;
 
-            lane1 = root.Q<VisualElement>("Lane1");
-            lane2 = root.Q<VisualElement>("Lane2");
-            lane3 = root.Q<VisualElement>("Lane3");
+            lanes = new VisualElement[LaneCount];
+            spawnButtons = new Button[2][];
+            spawnButtonClicked = new Action[2][];
+            spawnButtons[0] = new Button[LaneCount];
+            spawnButtons[1] = new Button[LaneCount];
+            spawnButtonClicked[0] = new Action[LaneCount];
+            spawnButtonClicked[1] = new Action[LaneCount];
             
-            lane1Player1SpawnButton = lane1.Q<Button>("Player1SpawnButton");
-            lane2Player1SpawnButton = lane2.Q<Button>("Player1SpawnButton");
-            lane3Player1SpawnButton = lane3.Q<Button>("Player1SpawnButton");
-            lane1Player2SpawnButton = lane1.Q<Button>("Player2SpawnButton");
-            lane2Player2SpawnButton = lane2.Q<Button>("Player2SpawnButton");
-            lane3Player2SpawnButton = lane3.Q<Button>("Player2SpawnButton");
+            for (var i = 0; i < lanes.Length; i++)
+            {
+                var laneCount = i + 1;
+                lanes[i] = root.Q <VisualElement>($"Lane{laneCount}");
+                spawnButtons[0][i] = lanes[i].Q<Button>($"Player1SpawnButton");
+                spawnButtons[1][i] = lanes[i].Q<Button>($"Player2SpawnButton");
+                spawnButtonClicked[0][i] = () => laneSystem.SpawnUnitFor(PlayerTag.Player1, laneCount);
+                spawnButtonClicked[1][i] = () => laneSystem.SpawnUnitFor(PlayerTag.Player2, laneCount);
+                spawnButtons[0][i].clicked += spawnButtonClicked[0][i];
+                spawnButtons[1][i].clicked += spawnButtonClicked[1][i];
+            }
 
 #if UNITY_IOS || UNITY_ANDROID || UNITY_WP_8_1
 #else
-            lane2Player1SpawnButton.AddToClassList("focused");
-            lane2Player2SpawnButton.AddToClassList("focused");
+            var midLaneIndex = LaneCount % 2 == 0 ? (LaneCount / 2) : (LaneCount - 1) / 2;
+            spawnButtons[0][midLaneIndex].AddToClassList("focused");
+            spawnButtons[1][midLaneIndex].AddToClassList("focused");
 #endif
-
-            lane1Player1SpawnButtonClicked = () => laneSystem.SpawnUnitFor(PlayerTag.Player1, 1);
-            lane2Player1SpawnButtonClicked = () => laneSystem.SpawnUnitFor(PlayerTag.Player1, 2);
-            lane3Player1SpawnButtonClicked = () => laneSystem.SpawnUnitFor(PlayerTag.Player1, 3);
-            lane1Player2SpawnButtonClicked = () => laneSystem.SpawnUnitFor(PlayerTag.Player2, 1);
-            lane2Player2SpawnButtonClicked = () => laneSystem.SpawnUnitFor(PlayerTag.Player2, 2);
-            lane3Player2SpawnButtonClicked = () => laneSystem.SpawnUnitFor(PlayerTag.Player2, 3);
-            
-            lane1Player1SpawnButton.clicked += lane1Player1SpawnButtonClicked;
-            lane2Player1SpawnButton.clicked += lane2Player1SpawnButtonClicked;
-            lane3Player1SpawnButton.clicked += lane3Player1SpawnButtonClicked;
-            lane1Player2SpawnButton.clicked += lane1Player2SpawnButtonClicked;
-            lane2Player2SpawnButton.clicked += lane2Player2SpawnButtonClicked;
-            lane3Player2SpawnButton.clicked += lane3Player2SpawnButtonClicked;
         }
 
         private void OnDestroy()
         {
-            lane1Player1SpawnButton.clicked -= lane1Player1SpawnButtonClicked;
-            lane2Player1SpawnButton.clicked -= lane2Player1SpawnButtonClicked;
-            lane3Player1SpawnButton.clicked -= lane3Player1SpawnButtonClicked;
-            lane1Player2SpawnButton.clicked -= lane1Player2SpawnButtonClicked;
-            lane2Player2SpawnButton.clicked -= lane2Player2SpawnButtonClicked;
-            lane3Player2SpawnButton.clicked -= lane3Player2SpawnButtonClicked;
+            for (var i = 0; i < lanes.Length; i++)
+            {
+                spawnButtons[0][i].clicked -= spawnButtonClicked[0][i];
+                spawnButtons[1][i].clicked -= spawnButtonClicked[1][i];
+            }
         }
 
         private void Update()
@@ -90,55 +75,43 @@ namespace DefaultNamespace
 
             if (keyboard.wKey.wasPressedThisFrame)
             {
-                if (lane2Player1SpawnButton.focusable && lane3Player1SpawnButton.ClassListContains("focused"))
+                var player1FocusedButton = FindFocusedButtonIndex(spawnButtons[0]);
+                if (player1FocusedButton != 0)
                 {
-                    SetButtonFocus(lane2Player1SpawnButton, lane3Player1SpawnButton);
-                }
-                else if (lane1Player1SpawnButton.focusable && lane2Player1SpawnButton.ClassListContains("focused"))
-                {
-                    SetButtonFocus(lane1Player1SpawnButton, lane2Player1SpawnButton);
+                    SetButtonFocus(spawnButtons[0][player1FocusedButton - 1], spawnButtons[0][player1FocusedButton]);
                 }
             }
             else if (keyboard.sKey.wasPressedThisFrame)
             {
-                if (lane3Player1SpawnButton.focusable && lane2Player1SpawnButton.ClassListContains("focused"))
+                var player1FocusedButton = FindFocusedButtonIndex(spawnButtons[0]);
+                if (player1FocusedButton != LaneCount - 1)
                 {
-                    SetButtonFocus(lane3Player1SpawnButton, lane2Player1SpawnButton);
-                }
-                else if (lane2Player1SpawnButton.focusable && lane1Player1SpawnButton.ClassListContains("focused"))
-                {
-                    SetButtonFocus(lane2Player1SpawnButton, lane1Player1SpawnButton);
+                    SetButtonFocus(spawnButtons[0][player1FocusedButton + 1], spawnButtons[0][player1FocusedButton]);
                 }
             }
             else if (keyboard.iKey.wasPressedThisFrame)
             {
-                if (lane2Player2SpawnButton.focusable && lane3Player2SpawnButton.ClassListContains("focused"))
+                var player2FocusedButton = FindFocusedButtonIndex(spawnButtons[1]);
+                if (player2FocusedButton != 0)
                 {
-                    SetButtonFocus(lane2Player2SpawnButton, lane3Player2SpawnButton);
-                }
-                else if (lane1Player2SpawnButton.focusable && lane2Player2SpawnButton.ClassListContains("focused"))
-                {
-                    SetButtonFocus(lane1Player2SpawnButton, lane2Player2SpawnButton);
+                    SetButtonFocus(spawnButtons[1][player2FocusedButton - 1], spawnButtons[1][player2FocusedButton]);
                 }
             }
             else if (keyboard.kKey.wasPressedThisFrame)
             {
-                if (lane3Player2SpawnButton.focusable && lane2Player2SpawnButton.ClassListContains("focused"))
+                var player2FocusedButton = FindFocusedButtonIndex(spawnButtons[1]);
+                if (player2FocusedButton != LaneCount - 1)
                 {
-                    SetButtonFocus(lane3Player2SpawnButton, lane2Player2SpawnButton);
-                }
-                else if (lane2Player2SpawnButton.focusable && lane1Player2SpawnButton.ClassListContains("focused"))
-                {
-                    SetButtonFocus(lane2Player2SpawnButton, lane1Player2SpawnButton);
+                    SetButtonFocus(spawnButtons[1][player2FocusedButton + 1], spawnButtons[1][player2FocusedButton]);
                 }
             }
             else if (keyboard.spaceKey.wasPressedThisFrame)
             {
-                ClickFocusedButton(lane1Player1SpawnButton, lane2Player1SpawnButton, lane3Player1SpawnButton);
+                ClickFocusedButton(spawnButtons[0]);
             }
             else if (keyboard.enterKey.wasPressedThisFrame)
             {
-                ClickFocusedButton(lane1Player2SpawnButton, lane2Player2SpawnButton, lane3Player2SpawnButton);
+                ClickFocusedButton(spawnButtons[1]);
             }
         }
 
@@ -151,13 +124,29 @@ namespace DefaultNamespace
 
         private static void ClickFocusedButton(params Button[] buttons)
         {
-            foreach (var button in buttons)
+            var button = buttons[FindFocusedButtonIndex(buttons)];
+            using var e = new NavigationSubmitEvent();
+            e.target = button;
+            button.SendEvent(e);
+            // foreach (var button in buttons)
+            // {
+            //     if (!button.ClassListContains("focused")) continue;
+            //     using var e = new NavigationSubmitEvent();
+            //     e.target = button;
+            //     button.SendEvent(e);
+            // }
+        }
+
+        private static int FindFocusedButtonIndex(params Button[] buttons)
+        {
+            for (var i = 0; i < buttons.Length; i++)
             {
+                var button = buttons[i];
                 if (!button.ClassListContains("focused")) continue;
-                using var e = new NavigationSubmitEvent();
-                e.target = button;
-                button.SendEvent(e);
+                return i;
             }
+
+            return -1;
         }
     }
 }
