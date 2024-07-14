9e27859b868568c28a5bc52f1e4fd0c26bb11eb2
SoonYu97 <yusoon.97417@gmail.com>
2024-07-14 14:38:26 +0800

    feat: add life ui

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 using Unity.Collections;
 using Unity.Entities;
 using UnityEngine;
@@ -19,7 +20,9 @@ namespace DefaultNamespace
         private VisualElement player2;
 
         private Label[] player1Units;
+        private Label player1Lives;
         private Label[] player2Units;
+        private Label player2Lives;
         
 
         private void Start()
@@ -34,6 +37,7 @@ namespace DefaultNamespace
             player1Units[2] = player1.Q<Label>("Unit3");
             player1Units[3] = player1.Q<Label>("Unit4");
             player1Units[4] = player1.Q<Label>("Unit5");
+            player1Lives = player1.Q<Label>("Life");
             
             player2 = root.Q<VisualElement>("Player2");
             player2Units = new Label[5];
@@ -42,6 +46,7 @@ namespace DefaultNamespace
             player2Units[2] = player2.Q<Label>("Unit3");
             player2Units[3] = player2.Q<Label>("Unit4");
             player2Units[4] = player2.Q<Label>("Unit5");
+            player2Lives = player2.Q<Label>("Life");
             
             var query = new EntityQueryBuilder(Allocator.Temp).WithAll<PlayerData>().Build(entityManager);
             entityArray = query.ToEntityArray(Allocator.Persistent);
@@ -62,14 +67,20 @@ namespace DefaultNamespace
 
         private void Update()
         {
-            ResetAllLabel();
-            foreach (var entity in entityArray)
-            {
-                if (entityManager.GetComponentData<PlayerData>(entity).Tag == PlayerTag.Player1)
-                    playerData1 = entityManager.GetComponentData<PlayerData>(entity);
-                if (entityManager.GetComponentData<PlayerData>(entity).Tag == PlayerTag.Player2)
-                    playerData2 = entityManager.GetComponentData<PlayerData>(entity);
-            }
+            ResetAllUnitsLabel();
+            RefreshPlayerData();
+            UpdateUnitsLabel();
+            UpdateLives();
+        }
+
+        private void UpdateLives()
+        {
+            player1Lives.text = playerData1.Lives.ToString(CultureInfo.InvariantCulture);
+            player2Lives.text = playerData2.Lives.ToString(CultureInfo.InvariantCulture);
+        }
+
+        private void UpdateUnitsLabel()
+        {
             for (var idx = 0; idx < playerData1.Queue.Length; idx++)
             {
                 var unitType = playerData1.Queue[idx];
@@ -81,6 +92,7 @@ namespace DefaultNamespace
                     _ => ""
                 };
             }
+
             for (var idx = 0; idx < playerData2.Queue.Length; idx++)
             {
                 var unitType = playerData2.Queue[idx];
@@ -94,7 +106,18 @@ namespace DefaultNamespace
             }
         }
 
-        private void ResetAllLabel()
+        private void RefreshPlayerData()
+        {
+            foreach (var entity in entityArray)
+            {
+                if (entityManager.GetComponentData<PlayerData>(entity).Tag == PlayerTag.Player1)
+                    playerData1 = entityManager.GetComponentData<PlayerData>(entity);
+                if (entityManager.GetComponentData<PlayerData>(entity).Tag == PlayerTag.Player2)
+                    playerData2 = entityManager.GetComponentData<PlayerData>(entity);
+            }
+        }
+
+        private void ResetAllUnitsLabel()
         {
             foreach (var label in player1Units)
             {
